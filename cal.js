84861e6b20e10e2d20b30eb033a32ff608ec3076
Himanshu <usrharsh@gmail.com>
2026-03-09 18:06:25 +0530

    Add cal.js with basic calculator functions

---
+++ cal.js
@@ -0,0 +1,22 @@
+// Basic Calculator Functions
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b !== 0) {
+        return a / b;
+    } else {
+        return 'Error: Division by zero';
+    }
+}
+
+module.exports = { add, subtract, multiply, divide };
